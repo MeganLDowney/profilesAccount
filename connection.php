78dfaab63aa633e900a343d453aae23f2a6bcd20
Megan Downey <132178948+MeganLDowney@users.noreply.github.com>
2023-06-29 20:45:45 -0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -9,9 +9,9 @@
 
 //Connect to the database - use these values if you are using my webserver, just change your db name to your own
 $host = "127.0.0.1"; // For most, just use 127.0.0.1 to indicate your local host which will work for my webserver
-$user = "megan_megan"; //Your database username Does not change
-$pass = "@Lbcc2023!"; // Your database user password
-$db = "megan_cosw30_finalweeks"; //Your database name you want to connect to - add your number to the end of this
+$user = ""; //Your database username Does not change
+$pass = ""; // Your database user password
+$db = ""; //Your database name you want to connect to - add your number to the end of this
 $port = 3306; //The port #. It is always 3306
 
 // Try to make a database connection
